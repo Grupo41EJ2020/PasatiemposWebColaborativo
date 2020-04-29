230b6eab25f999f294918d9f8d62212467bf52fa
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-04-29 08:22:18 -0500

    Trabajo LIIGabriel finalizado

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -16,5 +16,10 @@ namespace MVCLaboratorio.Controllers
             return View();
         }
 
+        public ActionResult LIIGabriel()
+        {
+            return View();
+        }
+
     }
 }
