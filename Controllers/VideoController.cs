65f67ba20ff29fd10951b3ea5781f9fb8f976156
Ingrid Mares Rangel <ingridmares17@gmail.com>
2020-05-06 14:35:48 -0500

    selec video finalizado

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -163,8 +163,32 @@ namespace MVCLaboratorio.Controllers
                 lstvideos.Add(datosVideo);
 
             }
-
             return View(lstvideos);
         }
+
+
+             public ActionResult ialemares()
+        {
+                 DataTable dtVideos = BaseHelper.ejecutarConsulta("sp_Video_ConsultarTodo", CommandType.StoredProcedure);
+
+                 List<Video> lstvideos = new List<Video>();
+
+                 foreach (DataRow item in dtVideos.Rows)
+                 {
+                     Video datosVideo = new Video();
+
+                     datosVideo.IdVideo = int.Parse(item["IdVideo"].ToString());
+                     datosVideo.Nombre= item["Nombre"].ToString();
+                     datosVideo.Url= item["Url"].ToString();
+                     datosVideo.FechaPublicacion = DateTime.Parse(item["FechaPublicacion"].ToString());
+
+                     lstvideos.Add(datosVideo);
+                 }
+                 
+                 return View(lstvideos);
+        }
+
+            
+        }
     }
-}
+
