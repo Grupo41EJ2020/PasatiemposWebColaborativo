4e3f89044333eaa66990f2b44b0c5a1bb5e64334
Andrea Guadalupe Parga Pereyra <andrea_parga01@hotmail.com>
2020-05-18 00:12:52 -0500

    Actualizar y Borrar terminado

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -2668,6 +2668,81 @@ namespace MVCLaboratorio.Controllers
             }
         }
 
+
+        public ActionResult AndreagppDelete(int id)
+        {
+            //Consultar informacion del Video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+
+            DataTable dtVideo = BaseHelper.ejecutarConsulta("sp_Video_ConsultarPorId", CommandType.StoredProcedure, parametros);
+
+            Video miVideo = new Video();
+
+            if (dtVideo.Rows.Count > 0)
+            {
+                miVideo.IdVideo = int.Parse(dtVideo.Rows[0]["IdVideo"].ToString());
+                miVideo.Nombre = dtVideo.Rows[0]["Nombre"].ToString();
+                miVideo.Url = dtVideo.Rows[0]["Url"].ToString();
+                miVideo.FechaPublicacion = DateTime.Parse(dtVideo.Rows[0]["FechaPublicacion"].ToString());
+                return View(miVideo);
+            }
+            else
+            {
+                return View("Error");
+            }
+       }
+
+        [HttpPost]
+        public ActionResult AndreagppDelete(int id, FormCollection frm)
+        {
+            //obtener info del video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+
+            BaseHelper.ejecutarConsulta("sp_Video_Eliminar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Andreagpp");
+        }
+
+        public ActionResult AndreagppEdit(int id)
+        {
+            //Consultar los datos del video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+
+            DataTable dtVideo = BaseHelper.ejecutarConsulta("sp_Video_ConsultarPorID", CommandType.StoredProcedure, parametros);
+
+            Video miVideo = new Video();
+
+            if (dtVideo.Rows.Count > 0)
+            {
+                miVideo.IdVideo = int.Parse(dtVideo.Rows[0]["IdVideo"].ToString());
+                miVideo.Nombre = dtVideo.Rows[0]["Nombre"].ToString();
+                miVideo.Url = dtVideo.Rows[0]["Url"].ToString();
+                miVideo.FechaPublicacion = DateTime.Parse(dtVideo.Rows[0]["FechaPublicacion"].ToString());
+                return View(miVideo);
+            }
+            else
+            {  //no encontrado 
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AndreagppEdit(int id, Video datos)
+        {
+            //Realizar el Update
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+            parametros.Add(new SqlParameter("@Nombre", datos.Nombre));
+            parametros.Add(new SqlParameter("@Url", datos.Url));
+            parametros.Add(new SqlParameter("@FechaPublicacion", datos.FechaPublicacion));
+
+            BaseHelper.ejecutarConsulta("sp_Video_Actualizar", CommandType.StoredProcedure, parametros);
+
+            return RedirectToAction("Andreagpp");
+        }
+        
         public ActionResult JAOG2000()
         {
             //obtener todos los videos
