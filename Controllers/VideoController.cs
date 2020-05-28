0ce64ffe2a9e0e47722327c4400eaf7f5dae099e
Jonatan Garcia Cavazos <jonatanrayados@gmail.com>
2020-05-28 02:02:05 -0500

    Finalice Practica

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -3614,6 +3614,24 @@ namespace MVCLaboratorio.Controllers
             BaseHelper.ejecutarConsulta("sp_Video_Actualizar", CommandType.StoredProcedure, parametros);
             return RedirectToAction("SirObzedat");
         }
+
+            public ActionResult JonatanGC7()
+        {
+            DataTable dtVideos = BaseHelper.ejecutarConsulta("sp_Video_ConsultarTodo", CommandType.StoredProcedure);
+            List<Video> lstvideos = new List<Video>();
+            foreach (DataRow item in dtVideos.Rows)
+            {
+                Video datosVideo = new Video();
+                datosVideo.IdVideo = int.Parse(item["IdVideo"].ToString());
+                datosVideo.Nombre = item["Nombre"].ToString();
+                datosVideo.Url = item["Url"].ToString();
+                datosVideo.FechaPublicacion = DateTime.Parse(item["FechaPublicacion"].ToString());
+
+                lstvideos.Add(datosVideo);
+
+            }
+            return View(lstvideos);
+        }
     }
 }
 
