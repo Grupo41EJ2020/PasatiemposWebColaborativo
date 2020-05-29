44d4d106693700d88d44749287995de9b15b6c4a
aboitesabril13 <56052366+aboitesabril13@users.noreply.github.com>
2020-05-28 21:10:24 -0500

    terminodedelete

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -3768,8 +3768,41 @@ namespace MVCLaboratorio.Controllers
                 }
             }
         }
+        public ActionResult aboitesabril13Delete(int id)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+
+            DataTable dtVideo = BaseHelper.ejecutarConsulta("sp_Video_ConsultarPorID", CommandType.StoredProcedure, parametros);
+
+            Video miVideo = new Video();
+
+            if (dtVideo.Rows.Count > 0)
+            {
+                miVideo.IdVideo = int.Parse(dtVideo.Rows[0]["IdVideo"].ToString());
+                miVideo.Nombre = dtVideo.Rows[0]["Nombre"].ToString();
+                miVideo.Url = dtVideo.Rows[0]["Url"].ToString();
+                miVideo.FechaPublicacion = DateTime.Parse(dtVideo.Rows[0]["FechaPublicacion"].ToString());
+                return View(miVideo);
+            }
+            else
+            {
+                return View("ERROR");
+            }
+        }
+         [HttpPost]
+        public ActionResult aboitesabril13Delete(int id, FormCollection frm)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+
+            BaseHelper.ejecutarConsulta("sp_Video_Eliminar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("aboitesabril13");
+        }
+      
+        }
     }
-}
+
 
     
 
